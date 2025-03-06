9c3ea99c68e6987bc932a4ee4a40af64873b3d3d
Yejun Su <goofan.su@gmail.com>
2025-03-06 10:52:38 +0800

    remove init-elfeed.el
    use miniflux instead

--- modules/init-elfeed.el
+++
@@ -1,9 +0,0 @@
-(use-package elfeed
-  :pin melpa
-  :bind ("C-c e" . elfeed)
-  :custom
-  (elfeed-feeds
-   '("https://sachachua.com/blog/category/emacs-news/feed/"
-     "https://world.hey.com/this.week.in.rails/feed.atom")))
-
-(provide 'init-elfeed)
