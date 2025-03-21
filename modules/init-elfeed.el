8a4b30bfdf37e7479f314d4a1131fa13349d7e19
Yejun Su <goofan.su@gmail.com>
2025-03-21 10:08:23 +0800

    update elfeeds

--- modules/init-elfeed.el
+++ modules/init-elfeed.el
@@ -3,6 +3,7 @@
   :bind ("C-c e" . elfeed)
   :custom
   (elfeed-feeds
-   '("https://protesilaos.com/codelog.xml")))
+   '("https://protesilaos.com/codelog.xml"
+     "https://karthinks.com/tags/emacs/index.xml")))
 
 (provide 'init-elfeed)
