961e911e4933667c5909e37bc6944a1e90bc0843
Yejun Su <goofan.su@gmail.com>
2025-03-27 23:31:58 +0800

    bind keycast-mode-line-mode

--- modules/init-modeline.el
+++ modules/init-modeline.el
@@ -30,6 +30,7 @@
 (use-package keycast
   :pin melpa
   :after prot-modeline
+  :bind (:map my-toggle-map ("k" . keycast-mode-line-mode))
   :custom
   (keycast-mode-line-format "%2s%k%c%R")
   (keycast-mode-line-insert-after 'prot-modeline-vc-branch)
