0d59c4db65fd02f6a1672cd484bf3224205239dc
Yejun Su <goofan.su@gmail.com>
2025-05-13 12:31:59 +0800

    simplify consult-gh configuration

--- modules/init-git.el
+++ modules/init-git.el
@@ -45,18 +45,14 @@
 (use-package consult-gh
   :pin melpa
   :after consult
+  :init
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-orgs-list)
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-repos-list)
   :bind ("C-c g g" . consult-gh)
   :custom
   (consult-gh-default-clone-directory my-src-directory)
-  (consult-gh-repo-action #'consult-gh--repo-browse-files-action)
-  (consult-gh-issue-action #'consult-gh--issue-view-action)
-  (consult-gh-pr-action #'consult-gh--pr-view-action)
-  (consult-gh-code-action #'consult-gh--code-view-action)
-  (consult-gh-file-action #'consult-gh--files-view-action)
   (consult-gh-large-file-warning-threshold 2500000)
   :config
-  (add-to-list 'savehist-additional-variables 'consult-gh--known-orgs-list)
-  (add-to-list 'savehist-additional-variables 'consult-gh--known-repos-list)
   (consult-gh-enable-default-keybindings))
 
 (use-package consult-gh-embark
