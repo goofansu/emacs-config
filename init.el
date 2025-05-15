8669116dac3c792328bc1f0874018889d161888c
Yejun Su <goofan.su@gmail.com>
2025-05-15 09:55:15 +0800

    add init-misc.el

--- init.el
+++ init.el
@@ -23,3 +23,6 @@
 (require 'init-programming)
 (require 'init-reading)
 (require 'init-writing)
+
+;; Miscellaneous
+(require 'init-misc)
---
+++ modules/init-misc.el
@@ -0,0 +1,5 @@
+(use-package 0x0
+  :pin melpa
+  :bind (:map embark-region-map ("U" . 0x0-upload-text)))
+
+(provide 'init-misc)
