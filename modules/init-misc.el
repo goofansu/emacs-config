671b225f7d085778a34a0c274b9cf0934c7fc48a
Yejun Su <goofan.su@gmail.com>
2025-05-24 16:09:51 +0800

    add osm

--- modules/init-misc.el
+++ modules/init-misc.el
@@ -2,4 +2,8 @@
   :pin melpa
   :bind (:map embark-region-map ("U" . 0x0-upload-text)))
 
+(use-package osm
+  :bind ("C-c o" . osm-prefix-map)
+  :custom (osm-home '(31.231271 121.470015 8)))
+
 (provide 'init-misc)
