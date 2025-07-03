aaf1d380eda91252f3c27758f600aaf5afa85bdc
Yejun Su <goofan.su@gmail.com>
2025-07-03 16:55:30 +0800

    changing to agentic coding tools
    The idea is to use only light-weight AI functionalities in Emacs, instead,
    use agentic coding tools such as claude-code and gemini-cli in terminal.
    
    Changes in this commit:
    - Change default model to claude-sonnet-4, as most of my work in Emacs is coding.
    - Remove unused models. I don't use Gemini in Emacs, but use gemini-cli instead.
    - Add presets and set models in them.
    - Remove MCP tools.

--- modules/init-gpt.el
+++ modules/init-gpt.el
@@ -34,40 +34,6 @@
                  :input-cost 0.4
                  :output-cost 1.6
                  :cutoff-date "2024-05")
-                (openai/gpt-4.1-nano
-                 :description "Fastest, most cost-effective GPT-4.1 model"
-                 :capabilities (media tool-use json url)
-                 :mime-types ("image/jpeg" "image/png" "image/gif" "image/webp")
-                 :context-window 1024
-                 :input-cost 0.10
-                 :output-cost 0.40
-                 :cutoff-date "2024-05")
-                (openai/o4-mini
-                 :description "Fast, effective reasoning with efficient performance in coding and visual tasks"
-                 :capabilities (reasoning media tool-use json url)
-                 :mime-types ("image/jpeg" "image/png" "image/gif" "image/webp")
-                 :context-window 200
-                 :input-cost 1.10
-                 :output-cost 4.40
-                 :cutoff-date "2024-05")
-                (google/gemini-2.5-flash
-                 :description "Best model in terms of price-performance, offering well-rounded capabilities."
-                 :capabilities (tool-use json media)
-                 :mime-types ("image/png" "image/jpeg" "image/webp" "image/heic" "image/heif"
-                              "application/pdf" "text/plain" "text/csv" "text/html")
-                 :context-window 1048 ; 65536 output token limit
-                 :input-cost 0.3
-                 :output-cost 2.50
-                 :cutoff-date "2025-01")
-                (google/gemini-2.5-pro
-                 :description "State-of-the-art thinking model, capable of reasoning over complex problems"
-                 :capabilities (tool-use json media)
-                 :mime-types ("image/png" "image/jpeg" "image/webp" "image/heic" "image/heif"
-                              "application/pdf" "text/plain" "text/csv" "text/html")
-                 :context-window 1048 ; 65536 output token limit
-                 :input-cost 1.25 ; 2.50 for >200k tokens
-                 :output-cost 10.00 ; 15 for >200k tokens
-                 :cutoff-date "2025-01")
                 )))
 
   :bind
@@ -84,7 +50,7 @@
 
   :config
   (setq gptel-backend gptel--openrouter
-        gptel-model 'openai/gpt-4.1)
+        gptel-model 'anthropic/claude-sonnet-4)
 
   (defun my/gptel-chat ()
     (interactive)
@@ -145,7 +111,7 @@ If region is active, use it as TEXT; otherwise prompt for input.
 Display the result in a side window with the content selected."
     (interactive "sTranslate text: ")
     (let ((gptel-backend gptel--openrouter)
-          (gptel-model 'openai/gpt-4.1))
+          (gptel-model 'openai/gpt-4.1-mini))
       (gptel-request text
         :system "Translate the provided text between English and
 Chinese (Mandarin). Return ONLY the completed translation without
@@ -156,118 +122,53 @@ translation reads naturally to native speakers."
         :callback #'my/gptel--callback-display-bottom)))
 
   ;; Tools
-  (setq gptel-tools
-        (list
-         (gptel-make-tool
-          :confirm t
-          :category "command"
-          :name "run_command"
-          :description "Run a command."
-          :args (list '( :name "command"
-                         :type string
-                         :description "Command to run."))
-          :function (lambda (command)
-                      (with-temp-message (format "Running command: %s" command)
-                        (shell-command-to-string command))))
+  (gptel-make-tool
+   :category "emacs"
+   :name "read_documentation"
+   :description "Read the documentation for a given function or variable"
+   :args (list '( :name "name"
+                  :type string
+                  :description "The name of the function or variable whose documentation is to be retrieved"))
+   :function (lambda (symbol)
+               (let ((sym (intern symbol)))
+                 (cond
+                  ((fboundp sym)
+                   (documentation sym))
+                  ((boundp sym)
+                   (documentation-property sym 'variable-documentation))
+                  (t
+                   (format "No documentation found for %s" symbol))))))
 
-         (gptel-make-tool
-          :category "emacs"
-          :name "read_buffer"
-          :description "Return the contents of an Emacs buffer"
-          :args (list '( :name "buffer"
-                         :type string
-                         :description "The name of the buffer whose contents are to be retrieved"))
-          :function (lambda (buffer)
-                      (unless (buffer-live-p (get-buffer buffer))
-                        (error "Error: buffer %s is not live." buffer))
-                      (with-current-buffer buffer
-                        (buffer-substring-no-properties (point-min) (point-max)))))
-
-         (gptel-make-tool
-          :category "emacs"
-          :name "read_documentation"
-          :description "Read the documentation for a given function or variable"
-          :args (list '( :name "name"
-                         :type string
-                         :description "The name of the function or variable whose documentation is to be retrieved"))
-          :function (lambda (symbol)
-                      (let ((sym (intern symbol)))
-                        (cond
-                         ((fboundp sym)
-                          (documentation sym))
-                         ((boundp sym)
-                          (documentation-property sym 'variable-documentation))
-                         (t
-                          (format "No documentation found for %s" symbol))))))
-
-         (gptel-make-tool
-          :category "filesystem"
-          :name "read_file"
-          :description "Read and display the contents of a file"
-          :args (list '( :name "filepath"
-                         :type string
-                         :description "Path to the file to read. Supports relative paths and ~."))
-          :function (lambda (filepath)
-                      (with-temp-buffer
-                        (insert-file-contents (expand-file-name filepath))
-                        (buffer-string))))
-
-         (gptel-make-tool
-          :category "web"
-          :name "read_url"
-          :description "Fetch and read the contents of a URL"
-          :args (list '( :name "url"
-                         :type string
-                         :description "The URL to read"))
-          :function (lambda (url)
-                      (with-current-buffer (url-retrieve-synchronously url)
-                        (goto-char (point-min))
-                        (forward-paragraph)
-                        (let ((dom (libxml-parse-html-region (point) (point-max))))
-                          (run-at-time 0 nil #'kill-buffer (current-buffer))
-                          (with-temp-buffer
-                            (shr-insert-document dom)
-                            (buffer-substring-no-properties (point-min) (point-max)))))))))
+  (gptel-make-tool
+   :category "web"
+   :name "read_url"
+   :description "Fetch and read the contents of a URL"
+   :args (list '( :name "url"
+                  :type string
+                  :description "The URL to read"))
+   :function (lambda (url)
+               (with-current-buffer (url-retrieve-synchronously url)
+                 (goto-char (point-min))
+                 (forward-paragraph)
+                 (let ((dom (libxml-parse-html-region (point) (point-max))))
+                   (run-at-time 0 nil #'kill-buffer (current-buffer))
+                   (with-temp-buffer
+                     (shr-insert-document dom)
+                     (buffer-substring-no-properties (point-min) (point-max)))))))
 
   ;; Presets
-  (gptel-make-preset 'default
-    :description "Default tools"
-    :backend "OpenRouter"
-    :model 'openai/gpt-4.1
-    :tools (mapcar #'gptel-tool-name gptel-tools))
-
   (gptel-make-preset 'emacs
     :description "Emacs documentation"
     :backend "OpenRouter"
     :model 'openai/gpt-4.1
-    :system "You are an Emacs expert, respond with examples."
+    :system "You are an Emacs expert, read_documentation and respond with examples."
     :tools '("read_documentation"))
 
   (gptel-make-preset 'web
-    :description "Search web"
+    :description "Web tools"
     :backend "OpenRouter"
     :model 'openai/gpt-4.1
-    :tools '("read_url"
-             "web_search_exa"))
-
-  (gptel-make-preset 'wiki
-    :description "Search Wikipedia"
-    :backend "OpenRouter"
-    :model 'openai/gpt-4
-    :system "Search wikipedia, respond with source links."
-    :tools '("read_url"
-             "wikipedia_search_exa"))
-
-  (gptel-make-preset 'github
-    :description "GitHub tasks"
-    :backend "OpenRouter"
-    :model 'openai/gpt-4.1
-    :tools '("get_issue"
-             "get_issue_comments"
-             "get_pull_request"
-             "get_pull_request_comments"
-             "get_pull_request_diff"
-             "get_pull_request_files"))
+    :tools '("read_url"))
   )
 
 (use-package gptel-quick
@@ -276,23 +177,5 @@ translation reads naturally to native speakers."
   :config
   (setq gptel-quick-backend gptel--openrouter
         gptel-quick-model 'openai/gpt-4.1-mini))
-
-(use-package mcp
-  :pin melpa
-  :after gptel
-  :custom
-  (mcp-hub-servers
-   `(("github" . ( :command "github-mcp-server"
-                   :args ("stdio")
-                   :env ( :GITHUB_PERSONAL_ACCESS_TOKEN ,(auth-source-pass-get 'secret "api-key/github")
-                          :GITHUB_TOOLSETS "issues,pull_requests")))
-
-     ("exa" . ( :command "npx"
-                :args ("-y", "exa-mcp-server", "--tools=web_search_exa,wikipedia_search_exa")
-                :env ( :EXA_API_KEY ,(auth-source-pass-get 'secret "api-key/exa-ai"))))
-     ))
-  :config
-  (require 'gptel-integrations)
-  (mcp-hub-start-all-server #'gptel-mcp-connect))
 
 (provide 'init-gpt)
