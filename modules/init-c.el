9b250bc7dadb3441b356fc4e6603bb949601df9f
Yejun Su <goofan.su@gmail.com>
2025-07-24 14:03:28 +0800

    unset c-ts-mode-indent-offset

--- modules/init-c.el
+++ modules/init-c.el
@@ -1,8 +1,6 @@
 (use-package c-ts-mode
   :ensure nil
   :hook (c-ts-mode . c-format-before-save)
-  :custom
-  (c-ts-mode-indent-offset 4)
   :config
   (defun c-format-before-save ()
     (add-hook 'before-save-hook #'eglot-format-buffer nil t)))
