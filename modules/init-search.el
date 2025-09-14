66b9ca7466058ee06842055bda6ff04420cfefee
Yejun Su <goofan.su@gmail.com>
2025-09-14 22:28:34 +0800

    always search devdocs website
    - also remove browser-hist

--- modules/init-search.el
+++ modules/init-search.el
@@ -1,25 +1,5 @@
-(use-package browser-hist
-  :pin melpa
-  :bind (:map search-map ("U" . browser-hist-search)))
-
 (use-package devdocs
-  :hook
-  (ruby-ts-mode . (lambda () (setq-local devdocs-current-docs '("ruby~3.3" "rails~7.2" "rails~8.0"))))
-  (elixir-ts-mode . (lambda () (setq-local devdocs-current-docs '("elixir~1.18" "phoenix"))))
-  ((web-mode haml-mode heex-ts-mode) . (lambda () (setq-local devdocs-current-docs '("html" "css" "javascript" "tailwindcss"))))
-  :bind
-  ( :map search-map
-    ("k" . devdocs-lookup)
-    ("K" . devdocs-search))
-  :config
-  (defun my/devdocs-install ()
-    "Download and install selected DevDocs documentations."
-    (interactive)
-    (let* ((available-docs (mapcar (lambda (doc) (alist-get 'slug doc))
-                                   (devdocs--available-docs)))
-           (selected-docs (completing-read-multiple "Install documentations: " available-docs)))
-      (dolist (doc selected-docs)
-        (devdocs-install doc)))))
+  :bind (:map search-map ("k" . devdocs-search)))
 
 (use-package dictionary
   :ensure nil
