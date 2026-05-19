c151bbf84c75837e9a102f6e55cc6343250c92e0
Yejun Su <goofan.su@gmail.com>
2026-05-19 19:40:33 +0800

    remove unused packages from init-ruby.el

--- modules/init-ruby.el
+++ modules/init-ruby.el
@@ -2,26 +2,4 @@
   :ensure nil
   :defer t)
 
-(use-package inf-ruby
-  :pin nongnu
-  :hook (ruby-ts-mode . inf-ruby-minor-mode)
-  :bind (:map inf-ruby-minor-mode-map ("C-c C-s" . inf-ruby-console-auto))
-  :custom
-  (inf-ruby-console-environment "development")
-  :config
-  ;; Reserve "C-c C-r" for `rubocop-mode-map'.
-  (unbind-key "C-c C-r" inf-ruby-minor-mode-map))
-
-(use-package bundler
-  :pin melpa
-  :defer t)
-
-(use-package rspec-mode
-  :pin melpa
-  :hook (ruby-ts-mode rspec-compilation-mode))
-
-(use-package rubocop
-  :pin nongnu
-  :hook ruby-ts-mode)
-
 (provide 'init-ruby)
