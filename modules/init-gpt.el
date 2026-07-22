61275ddbb0bb7b3d0a9a75587f56865070d26e6e
Yejun Su <goofan.su@gmail.com>
2026-07-22 11:57:04 +0800

    Revert "use llama-cpp"
    This reverts commit da4643160b4a05265d9b6d146fd49df7d2faeac8.

--- modules/init-gpt.el
+++ modules/init-gpt.el
@@ -22,12 +22,13 @@
   (setf (alist-get 'markdown-mode gptel-prompt-prefix-alist) "**User**\n")
   (setf (alist-get 'markdown-mode gptel-response-prefix-alist) "**Assistant**\n")
 
-  (setq gptel-model 'unsloth/Qwen3.5-9B-GGUF:Q4_K_M
-        gptel-backend (gptel-make-openai "llama-cpp"
-                        :stream t
+  (setq gptel-model 'qwen/qwen3.5-9b
+        gptel-backend (gptel-make-openai "LM Studio"
                         :protocol "http"
-                        :host "localhost:8080"
-                        :models '(unsloth/Qwen3.5-9B-GGUF:Q4_K_M)))
+                        :host "127.0.0.1:1234"
+                        :endpoint "/v1/chat/completions"
+                        :stream t
+                        :models '(qwen/qwen3.5-9b)))
 
   (defun my/gptel-buffer-names ()
     "Return the names of buffers where `gptel-mode' is active."
