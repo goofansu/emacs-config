10722f1e610d42a13b2a0ce6b6f84a128dba902d
Yejun Su <goofan.su@gmail.com>
2026-08-09 22:51:35 +0800

    add my/homebrew-formulae-insert
    extract my/homebrew-packages-insert function to reuse

--- modules/init-nix.el
+++ modules/init-nix.el
@@ -1,18 +1,26 @@
 (use-package nix-mode
   :pin nongnu
   :hook (before-save . apheleia-mode)
-  :bind (:map my-insert-map ("n n" . my/homebrew-casks-insert))
+  :bind (:map my-insert-map
+              ("n n" . my/homebrew-casks-insert)
+              ("n f" . my/homebrew-formulae-insert))
   :config
+  (defun my/homebrew-packages-insert (kind)
+    "Insert currently installed Homebrew packages of KIND at point."
+    (let* ((output (string-trim
+                    (shell-command-to-string (format "brew list --%s" kind))))
+           (packages (unless (string-empty-p output)
+                       (split-string output "\n")))
+           (quoted-packages
+            (mapcar (lambda (package) (format "\"%s\"" package)) packages)))
+      (insert (format "[ %s ];" (string-join quoted-packages " ")))))
   (defun my/homebrew-casks-insert ()
     "Insert currently installed Homebrew Casks at point."
     (interactive)
-    (let* ((casks-output (string-trim
-                          (shell-command-to-string "brew list --cask")))
-           (casks-list (when (not (string-empty-p casks-output))
-                         (split-string casks-output "\n")))
-           (quoted-casks (mapcar (lambda (cask) (format "\"%s\"" cask))
-                                 casks-list))
-           (result (format "[ %s ];" (string-join quoted-casks " "))))
-      (insert result))))
+    (my/homebrew-packages-insert "cask"))
+  (defun my/homebrew-formulae-insert ()
+    "Insert currently installed Homebrew Formulae at point."
+    (interactive)
+    (my/homebrew-packages-insert "formulae")))
 
 (provide 'init-nix)
