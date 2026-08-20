68d8040ad46267c275afc42bf6d07852b09291bf
Yejun Su <goofan.su@gmail.com>
2026-08-20 17:44:29 +0800

    dictionary-lookup-definition -> dictionary-search

--- modules/init-search.el
+++ modules/init-search.el
@@ -1,7 +1,7 @@
 (use-package dictionary
   :ensure nil
   :bind
-  (:map search-map ("d" . dictionary-lookup-definition))
+  (:map search-map ("d" . dictionary-search))
   :custom
   (dictionary-server "dict.org")
   (dictionary-default-popup-strategy "lev")
