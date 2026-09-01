ce4e88793154c7c0343560465aea8874718784a5
Yejun Su <goofan.su@gmail.com>
2026-09-01 11:33:52 +0800

    Revert "remove denote-open-or-create-with-command keybinding"
    This reverts commit 53b71fcd22b1e59c351deec2466b91cd50314f62.

--- modules/init-writing.el
+++ modules/init-writing.el
@@ -92,6 +92,7 @@
   :hook (dired-mode . denote-dired-mode)
   :bind
   (("C-c n n" . denote-open-or-create)
+   ("C-c n N" . denote-open-or-create-with-command)
    ("C-c n o" . denote-sort-dired)
    ("C-c n r" . denote-rename-file)
    :map text-mode-map
